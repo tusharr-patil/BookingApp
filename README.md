620e6782b7db865030fbbe4619ea812a72e2d581
tusharr-patil <patiltusharvikas@gmail.com>
2023-07-02 19:32:55 +0530

    add styling

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bookings and Reservations
+
+- Built in Go version 1.20
+- Uses chi router
+
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,7 +14,12 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(middleware.Recoverer)
 	mux.Use(CsrfHandler)
 	mux.Use(SessionLoad)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
